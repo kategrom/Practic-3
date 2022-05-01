b948550715725cca51b686d9641afde6d0e7a003
kategrom <95121842+kategrom@users.noreply.github.com>
2022-05-01 13:25:51 +0300

    Add files via upload

---
+++ practic3.js
@@ -0,0 +1,83 @@
+'use strict'
+
+// Task 1
+
+//Integer(Int) = ціле число
+
+// const k1 = parseInt("5px");
+// console.log(k1);
+
+// const k2 = parseInt("12djd334");
+// console.log(k2);
+
+// const k3 = parseInt("12.45asdwe12");
+// console.log(k3);
+
+// const k4 = parseInt("qwqweeewq");
+// console.log(k4);
+
+/*
+* console.log(k1, k2, k3, k4)
+*/
+
+// Task 2
+
+// parseFloat = дробове число
+
+//  const a1 = parseFloat("5px");
+//  console.log(a1);
+
+//  const a2 = parseFloat("12djd334");
+//  console.log(a2);
+
+//  const a3 = parseFloat("12.45asdwe12");
+//  console.log(a3);
+
+//  const a4 = parseFloat("qwqweeewq");
+//  console.log(a4);
+
+
+// Task 3
+
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+// Task 4
+
+//random() * (max - min) + min;
+// const random = Math.random() * (19 - 3) + 3;
+// console.log(random);
+
+
+// Task 5
+
+// const result = 5 + 5 + "5";
+// console.log(result, typeof result);
+
+
+// Task 6
+
+// const email = "kategrom@gmail.com";
+
+// const isValid = email.includes("@");
+// console.log(isValid);
+
+// const number = email.length;
+// console.log(number);
+
+
+// Task 7
+
+const a = "My ";
+const b = "name ";
+const c = "is ";
+const fullName = a + b + c + "Kate"
+console.log(fullName);
+
+
+// Task 8
+
+// const userName = "Kattya";
+// const payment = 400;
+// const message = 'Дякуємо, ${userName}! До сплати ${payment} гривень';
+// alert(message);
